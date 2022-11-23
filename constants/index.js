d7dc9e2e56d6e15dd77ec9c0b8b388343a981857
Andrey Amelin <more207@mail.ru>
2022-11-23 22:40:55 +0300

    fix: дополнить CORS исключения

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,8 @@ const CAST_ERROR = 'CastError';
 const REGEX_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
 const VALIDATION_ERROR = 'Validation failed';
 const allowedCors = [
+  'https://amelin.movies.nomoredomains.club',
+  'http://amelin.movies.nomoredomains.club',
   'http://localhost:3000',
 ];
 
