7f1295ef0d36854b2e1c87d5b5843118157468e6
Andrey Amelin <more207@mail.ru>
2022-12-26 22:05:06 +0300

    fix: добавь айди пользователя в ответе на логин

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,7 @@ module.exports.login = (req, res, next) => {
           sameSite: 'None',
           secure: true,
         })
-        .send({ userInfo: { name: user.name, email: user.email }, message: 'Пользователь успешно авторизирован.' });
+        .send({ userInfo: { _id: user._id, email: user.email, name: user.name }, message: 'Пользователь успешно авторизирован.' });
     })
     .catch(next);
 };
