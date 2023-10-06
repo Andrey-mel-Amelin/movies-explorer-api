957d1dabae57942002d3171764feb90dc1b7bdef
Амелин Андрей Александрович <amelin_a_qschool@qsoft.ru>
2023-10-06 04:20:51 +0300

    fix: vercel

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,7 @@ const CAST_ERROR = 'CastError';
 const REGEX_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
 const VALIDATION_ERROR = 'Validation failed';
 const allowedCors = [
-  'https://movies-explorer-frontend-bay.vercel.app/',
+  'https://movies-explorer-frontend-bay.vercel.app',
   'http://localhost:3000',
 ];
 
